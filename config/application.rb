61d466836478b833903507bab18e621077c825ee
daveed <david.alphen@gmail.com>
2017-09-14 10:05:43 -0700

    adding lib and validators folders to load path

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module PlatformTest
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths += Dir["#{Rails.root}/lib/**/"]
+    config.autoload_paths += Dir["#{Rails.root}/app/validators/"]
   end
 end
