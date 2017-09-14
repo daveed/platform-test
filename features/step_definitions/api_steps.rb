8ecb9ea1d05f832a22c529ef5ffe5993688ffec1
daveed <david.alphen@gmail.com>
2017-09-14 15:50:35 -0700

    testing login endpoint response token

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -19,6 +19,10 @@ When(/^I do a POST request to "([^"]*)" with:$/) do |path, body|
   post(path, body)
 end
 
+Then(/^the response token should be "([^"]*)"$/) do |body|
+  expect(body).to eq("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxfQ.iuSIB-Zr-xT1DcXwoRGa67pBZnO54p-pI7iRbtJfBgKXMACFjs0dPnvJUsgnVwlZFSpN8CbRhrZ-2mraZ14zVw")
+end
+
 When(/^I do a DELETE request to \/v(\d+)\/logout$/) do |arg1|
   pending
 end
--- features/users/login_user.feature
+++ features/users/login_user.feature
@@ -10,3 +10,4 @@ Feature: Log a user in
       }
       """
     Then the response status should be 200
+    And the response token should be "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxfQ.iuSIB-Zr-xT1DcXwoRGa67pBZnO54p-pI7iRbtJfBgKXMACFjs0dPnvJUsgnVwlZFSpN8CbRhrZ-2mraZ14zVw"
