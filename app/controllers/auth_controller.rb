8a1b5925adc10e5203dc597ac93e05c727f67ab7
daveed <david.alphen@gmail.com>
2017-09-14 11:06:57 -0700

    login endpoint authenticates a user and returns a jwt token

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,23 @@
 class AuthController < ApplicationController
-  def create; end
+  # include JwtUtil
+
+  def create
+    user = User.find_by(email: params[:email])
+    authenticated_user = user.authenticate(params[:password]) if user
+
+    if authenticated_user
+      token = JwtUtil.encode(user_id: user.id)
+      render json: { token: token }
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def user_params
+    params.fetch(:email, :password)
+  end
 end
