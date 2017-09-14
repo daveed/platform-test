6841c034ec9d48ef2444999617410e615a968204
daveed <david.alphen@gmail.com>
2017-09-14 10:06:11 -0700

    routes endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: %i[create update destroy]
+
+  scope :auth do
+    post   'login',  to: 'auth#create'
+    delete 'logout', to: 'auth#destroy'
+  end
 end
