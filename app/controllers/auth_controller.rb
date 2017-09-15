c40525c685c81765ade36165e52fa14f632547a1
daveed <david.alphen@gmail.com>
2017-09-14 17:13:05 -0700

    using symbols for params

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,7 @@
 class AuthController < ApplicationController
   def create
-    user = User.find_by(email: auth_params['email'])
-    authenticated_user = user.authenticate(auth_params['password']) if user
+    user = User.find_by(email: auth_params[:email])
+    authenticated_user = user.authenticate(auth_params[:password]) if user
 
     if authenticated_user
       token = JwtUtil.encode(user_id: user.id)
